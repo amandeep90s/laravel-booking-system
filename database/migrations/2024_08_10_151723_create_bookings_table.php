2164b9623c9db3b23bf33b881a08cfe72000dcee
Amandeep Singh <programmeramandeep@gmail.com>
2024-08-10 21:00:35 +0530

    feat(booking): booking table migration

--- database/migrations/2024_08_10_151723_create_bookings_table.php
+++ database/migrations/2024_08_10_151723_create_bookings_table.php
@@ -13,6 +13,19 @@ return new class extends Migration
     {
         Schema::create('bookings', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 100);
+            $table->string('aadhaarNumber', 20);
+            $table->string('mobileNumber', 20);
+            $table->string('address');
+            $table->string('state', 100)->nullable();
+            $table->string('district', 100);
+            $table->string('block', 100)->nullable();
+            $table->integer('numberOfVisitors');
+            $table->string('purposeSubject');
+            $table->text('purposeDescription')->nullable();
+            $table->dateTime('visitDate');
+            $table->text('guestsList')->nullable();
+            $table->softDeletes();
             $table->timestamps();
         });
     }
