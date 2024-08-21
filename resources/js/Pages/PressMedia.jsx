98ae4ccb5f02aed650927bb2358fe65b658ecfbc
Amandeep Singh <programmeramandeep@gmail.com>
2024-08-21 23:16:29 +0530

    chore: Added callout component

--- resources/js/Pages/PressMedia.jsx
+++ resources/js/Pages/PressMedia.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Head } from '@inertiajs/react';
 import HomeLayout from '@/Layouts/HomeLayout.jsx';
+import Callout from '@/Components/Callout.jsx';
 
 const PressMedia = () => {
   return (
     <HomeLayout>
       <Head title="Press Media" />
+      <Callout title="Press / Media" link={route('press-media')} />
       <div>
         <h1>Coming Soon</h1>
       </div>
