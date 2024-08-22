4b201b4c6813c53c206fb7c771499f842eff931e
Amandeep Singh <programmeramandeep@gmail.com>
2024-08-22 21:23:24 +0530

    chore: Update Vite configuration and dependencies

--- resources/js/Layouts/HomeLayout.jsx
+++ resources/js/Layouts/HomeLayout.jsx
@@ -1,7 +1,7 @@
+import Footer from '@/Components/Footer.jsx';
 import Menu from '@/Components/Menu';
 import MobileMenu from '@/Components/MobileMenu';
 import { useEffect } from 'react';
-import Footer from '@/Components/Footer.jsx';
 
 export default function HomeLayout({ children }) {
   useEffect(() => {
@@ -18,65 +18,35 @@ export default function HomeLayout({ children }) {
       '/js/main.js',
     ];
 
-    // Function to dynamically load scripts
     const loadScripts = async () => {
       const loadedScripts = [];
       for (const src of scripts) {
-        const script = document.createElement('script');
-        script.src = src;
-        script.async = true;
-        document.body.appendChild(script);
-        loadedScripts.push(script);
+        if (!document.querySelector(`script[src="${src}"]`)) {
+          const script = document.createElement('script');
+          script.src = src;
+          script.async = true;
+          document.body.appendChild(script);
+          loadedScripts.push(script);
 
-        // Wait for each script to load
-        await new Promise((resolve) => {
-          script.onload = resolve;
-        });
+          await new Promise((resolve) => {
+            script.onload = resolve;
+          });
+        }
       }
 
-      // Initialize AOS after scripts are loaded
       if (window.AOS) {
         window.AOS.init();
+      }
+      if (window.ScrollSmoother) {
+        ScrollSmoother.create({
+          wrapper: '#smooth-wrapper',
+          content: '#smooth-content',
+        });
       }
 
       return loadedScripts;
     };
-
-    let loadedScripts = [];
-    loadScripts().then((scripts) => (loadedScripts = scripts));
-
-    // Cleanup on component unmount
-    return () => {
-      loadedScripts.forEach((script) => {
-        document.body.removeChild(script);
-      });
-    };
-  }, []);
-
-  useEffect(() => {
-    const cssFiles = ['/css/swiper-bundle.min.css', '/css/aos.css', '/css/output.css', '/css/style.css'];
-
-    // Function to dynamically load CSS files
-    const loadCSSFiles = () => {
-      const loadedLinks = [];
-      cssFiles.forEach((href) => {
-        const link = document.createElement('link');
-        link.rel = 'stylesheet';
-        link.href = href;
-        document.head.appendChild(link);
-        loadedLinks.push(link);
-      });
-      return loadedLinks;
-    };
-
-    const loadedLinks = loadCSSFiles();
-
-    // Cleanup on component unmount
-    return () => {
-      loadedLinks.forEach((link) => {
-        document.head.removeChild(link);
-      });
-    };
+    loadScripts();
   }, []);
 
   return (
