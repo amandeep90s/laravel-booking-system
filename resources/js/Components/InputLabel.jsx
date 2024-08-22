7fccd6b43604ebc3908ade1d02a9a35a4f0e6e8e
Amandeep Singh <programmeramandeep@gmail.com>
2024-08-22 23:23:43 +0530

    chore: Add required indicator to InputLabel component

--- resources/js/Components/InputLabel.jsx
+++ resources/js/Components/InputLabel.jsx
@@ -1,7 +1,8 @@
-export default function InputLabel({ value, className = '', children, ...props }) {
+export default function InputLabel({ value, required, className = '', children, ...props }) {
   return (
     <label {...props} className={`block text-sm font-medium text-gray-700 ` + className}>
       {value ? value : children}
+      {required && <span className="text-red-500">*</span>}
     </label>
   );
 }
