34a40edc1998928da27fc4f823c3f7c632a1dc6b
Amandeep Singh <programmeramandeep@gmail.com>
2024-08-27 21:50:34 +0530

    feat: Add super admin seeder

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,10 +24,10 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'name' => env('SUPER_ADMIN_NAME') ?? fake()->name(),
+            'email' => env('SUPER_ADMIN_EMAIL') ?? fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
+            'password' => static::$password ??= Hash::make(env('SUPER_ADMIN_PASSWORD') ?? 'password'),
             'remember_token' => Str::random(10),
         ];
     }
@@ -37,8 +37,10 @@ class UserFactory extends Factory
      */
     public function unverified(): static
     {
-        return $this->state(fn (array $attributes) => [
-            'email_verified_at' => null,
-        ]);
+        return $this->state(
+            fn (array $attributes) => [
+                'email_verified_at' => null,
+            ]
+        );
     }
 }
