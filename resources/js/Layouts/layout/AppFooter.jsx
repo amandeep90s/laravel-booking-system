2c30b5fd92f9c6a46de296d3eaaf25bb3519cb3b
Amandeep Singh <programmeramandeep@gmail.com>
2024-08-31 14:03:45 +0530

    feat: Add AppSidebar, AppConfigButton, AppFooter, MenuContext, AppMenu, DashboardInfoCard, LayoutContext, and AppTopbar components

---
+++ resources/js/Layouts/layout/AppFooter.jsx
@@ -0,0 +1,14 @@
+import React, { useContext } from 'react';
+
+const AppFooter = () => {
+  return (
+    <div className="layout-footer">
+      <span>
+        Made with <span className="text-red-500">&#10084;&#65039;</span> by
+      </span>
+      <span className="ml-2 font-medium">boadusamuel & team</span>
+    </div>
+  );
+};
+
+export default AppFooter;
